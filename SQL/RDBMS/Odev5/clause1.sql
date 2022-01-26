ec06f9d01dc8f95e4396f387fe50d332446665aa
gyildirimm <goktugyildirim22@gmail.com>
2022-01-26 17:07:52 +0300

    -Odev5

---
+++ SQL/RDBMS/Odev5/clause1.sql
@@ -0,0 +1,4 @@
+dvdrental=# SELECT * FROM film
+dvdrental-# WHERE title LIKE '%n'
+dvdrental-# ORDER BY length DESC
+dvdrental-# LIMIT 5;  
---
+++ SQL/RDBMS/Odev5/clause2.sql
@@ -0,0 +1,5 @@
+dvdrental=# SELECT * FROM film
+dvdrental-# WHERE title LIKE '%n'
+dvdrental-# ORDER BY length
+dvdrental-# OFFSET 5
+dvdrental-# LIMIT 5;
