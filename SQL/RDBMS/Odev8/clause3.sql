6a2909c3e77e4c02ac4b532bfe6fc540564730e2
gyildirimm <goktugyildirim22@gmail.com>
2022-01-26 17:27:36 +0300

    -SQL Odev8

---
+++ SQL/RDBMS/Odev8/clause3.sql
@@ -0,0 +1,8 @@
+UPDATE employee SET name = 'KObE' WHERE ID = 15;
+UPDATE employee SET name = 'LEbRoN' , birthday='02-12-1548' WHERE id = 2;
+UPDATE employee SET email = 'lbkobe@Jorjames.com' WHERE id = 3;
+UPDATE employee SET birthday = '01-01-1891' WHERE id = 10;
+UPDATE employee SET name = 'AT' WHERE id =45;
+
+SELECT * FROM employee
+ORDER BY id ASC;
---
+++ SQL/RDBMS/Odev8/clause4.sql
@@ -0,0 +1,5 @@
+DELETE FROM employee WHERE id = 1;
+DELETE FROM employee WHERE name = 'LEbRoN';
+DELETE FROM employee WHERE email = 'aaylott3@symantec.com';
+DELETE FROM employee WHERE birthday = '1548-12-02';
+DELETE FROM employee WHERE birthday = '1957-09-28' AND name = 'Adina';
